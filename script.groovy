e039ef3a1d0431ad8a986fbf086af619e32c6540
Ilia Falaleev <43509464+ilka95@users.noreply.github.com>
2022-03-19 16:14:14 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,10 @@
 pipeline {
   agent { label 'мастер' }
   stages {
-    stage('query') {
+    stage('Install MySQL') {
+      git clone https://github.com/jenkinsci/database-mysql-plugin.git
+    }
+    stage('Run query') {
       steps {
         sh(script:'''
           mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam < query.sql;
